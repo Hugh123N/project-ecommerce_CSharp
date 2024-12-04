9178a374000e91aed84bb993a7e9b4653e352026
Farid-Mejia <farid.mejia.tj@gmail.com>
2024-12-03 22:38:04 -0500

    Ya funciona conectado a BD, deriva al index de momento; Pero el nav tiene que cambiar para que en vez de usar el asp-controller='Home' use el del mismo Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proyecto_ecommerce_.NET_MVC_.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace proyecto_ecommerce_.NET_MVC_.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly EcommerceCursoContext _context;
+        public LoginController(EcommerceCursoContext context) 
+        { 
+            _context = context; 
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                TempData["ErrorMessage"] = "Debe completar ambos campos.";
+                return View("Index");
+            }
+            var user = await _context.Usuarios.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+
+            if (user == null)
+            {
+                TempData["ErrorMessage"] = "Usuario o contraseña incorrectos.";
+                return View("Index");
+            } // Logica para guardar la sesión del usuario puede ser agregada aquí.
+
+            TempData["SuccessMessage"] = "Inicio de sesión exitoso.";
+            return RedirectToAction("Index", "Home");
+
+        }
+
+
+    }
+}
