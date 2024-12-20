525dc399a3d32e451a08dfd73b7fedae7ea32654
Hugh123N <hughhnp@gmail.com>
2024-12-20 11:00:05 -0500

    implementacion BaseController para datos generales, cambio de id Session a id Cookies para un mejor manejo, muestra datos de usuario en la vista y el numero de productos en carrito

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace proyecto_ecommerce_.NET_MVC_.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            ViewBag.CarritoCount = HomeController.detalles.Count;
+            base.OnActionExecuted(context);
+        }
+    }
+}
